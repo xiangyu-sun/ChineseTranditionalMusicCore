6a64929d70e883dea694119f0acf00c46f9f34e0
Xiangyu Sun <luc.alexander.sun@icloud.com>
2021-02-08 17:15:51 +0800

    push the real code

--- Sources/ChineseTranditionalMusicCore/ChineseTranditionalMusicCore.swift
+++ Sources/ChineseTranditionalMusicCore/ChineseTranditionalMusicCore.swift
@@ -1,3 +1,17 @@
-struct ChineseTranditionalMusicCore {
-    var text = "Hello, World!"
+public enum 階名 {
+    case 宮
+    case 商
+    case 角
+    case 徵
+    case 羽
+}
+
+public enum 變聲 {
+    case 清(階名)
+    case 變(階名)
+    
+    static let 清角 = 變聲.清(.角)
+    static let 變徵 = 變聲.變(.徵)
+    static let 清羽 = 變聲.清(.羽)
+    static let 變宮 = 變聲.清(.宮)
 }
---
+++ Sources/ChineseTranditionalMusicCore/Lv.swift
@@ -0,0 +1,14 @@
+public enum 律呂 {
+    case 黃鐘
+    case 大呂
+    case 太簇
+    case 夾鐘
+    case 姑洗
+    case 仲呂
+    case 蕤賓
+    case 林鐘
+    case 夷則
+    case 南呂
+    case 無射
+    case 應鐘
+}
