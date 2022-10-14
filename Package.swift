de65b583fc7702a0ca8f92063da91bc2e57f103d
Xiangyu Sun <xiangyu.sun@glovoapp.com>
2022-10-14 17:21:03 +0200

    update dependencies

--- Package.swift
+++ Package.swift
@@ -16,14 +16,16 @@ let package = Package(
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
-        // .package(url: /* package url */, from: "1.0.0"),
+      .package(url: "git@github.com:xiangyu-sun/ChineseAstrologyCalendar.git", .branchItem("master"))
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "ChineseTranditionalMusicCore",
-            dependencies: []),
+            dependencies: [
+              .product(name: "ChineseAstrologyCalendar", package: "ChineseAstrologyCalendar")
+            ]),
         .testTarget(
             name: "ChineseTranditionalMusicCoreTests",
             dependencies: ["ChineseTranditionalMusicCore"]),
--- Sources/ChineseTranditionalMusicCore/Lv.swift
+++ Sources/ChineseTranditionalMusicCore/Lv.swift
@@ -1,3 +1,5 @@
+import ChineseAstrologyCalendar
+
 public enum 律呂: String, CaseIterable {
     case 黃鐘
     case 大呂
@@ -11,4 +13,34 @@ public enum 律呂: String, CaseIterable {
     case 南呂
     case 無射
     case 應鐘
+  
+  var dizhi: Dizhi {
+    switch self {
+    case .黃鐘:
+      return .yin
+    case .大呂:
+      return .mao
+    case .太簇:
+      return .chen
+    case .夾鐘:
+      return .si
+    case .姑洗:
+      return .wu
+    case .仲呂:
+      return .wei
+    case .蕤賓:
+      return .shen
+    case .林鐘:
+      return .you
+    case .夷則:
+      return .xu
+    case .南呂:
+      return .hai
+    case .無射:
+      return .zi
+    case .應鐘:
+      return .chou
+    }
+  }
+  
 }
