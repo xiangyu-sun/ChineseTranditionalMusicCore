6667922e134a3b199d66f409664de18291c5b5fc
Xiangyu Sun <xiangyu.sun@glovoapp.com>
2023-07-28 21:10:27 +0200

    add min watchOS version

--- Package.swift
+++ Package.swift
@@ -6,7 +6,7 @@ import PackageDescription
 let package = Package(
     name: "ChineseTranditionalMusicCore",
     platforms: [
-            .macOS(.v10_14), .iOS(.v13), .tvOS(.v13)
+            .macOS(.v10_14), .iOS(.v13), .tvOS(.v13), .watchOS(.v5)
         ],
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
